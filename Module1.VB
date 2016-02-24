db3d81c36fc6f09a9e6c6ea86050c167d8a092f8
KonstantinAvdeenko <AvdeenkoKonstantin1995@gmail.com>
2016-02-24 13:04:23 +0400

    Add my finding consecutive negative numbers in array elements

---
+++ Module1.VB
@@ -0,0 +1,26 @@
+﻿Private Sub CommandButton1_Click()
+'каждому элементу массива присваивается рандомное положительное или отрицательное число'
+For i = 1 To 30
+Cells(1, i) = Int((100 * Rnd) - 50)
+Next i
+End Sub
+
+Private Sub CommandButton2_Click()
+'подсчет максимального количества подряд идущих отрицательных элементов массива'
+For i = 1 To 30
+If Cells(1, i) < 0 Then
+k = k + 1
+Else
+k = 0
+End If
+If n < k Then
+n = k
+End If
+Next i
+MsgBox (n)
+End Sub
+
+Private Sub CommandButton3_Click()
+'закрытие формы'
+UserForm1.Hide
+End Sub
---
+++ READ_ME_ENG.md
@@ -0,0 +1,8 @@
+﻿This program is in the VBA programming language,
+calculates the means of the VBA language Content of a contract of contracting negative elements
+in an integer array of length 30.
+Main module Module 1 contains the following main functions of the corresponding main buttons:
+1) array button - fills array elements with random positive and negative values;
+2) answer button - calculates the number of participants of the contracting negative elements
+in an already filled integer array;
+3) end button - closes the form.
